a636fd5fae0f5f12a3ac0054445901db872c169e
Ngirfanul Mangali <metalqwerty0@gmail.com>
2024-10-02 20:59:58 +0700

    index.html

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sora: ['Sora'],
+      },
+      lineHeight: {
+        '56': '3.5rem',
+      }
+  },
+  plugins: [],
+}
+}
